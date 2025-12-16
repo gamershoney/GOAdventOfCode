c772a7cd8865db78ca934bbddf8c1f738f7dbb76
gamershoney <gbrycesanders@gmail.com>
2025-12-15 20:03:30 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var file string = "read.txt"
+
+var cv int = 0
+var counter int = 0
+
+func main() {
+	items, err := os.ReadFile(file)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	buf := bytes.Lines(items)
+
+	for line := range buf {
+		str := strings.Split(string(line), "")
+		dir := str[0]
+
+		num := strings.Join(str[1:len(str)], "")
+		val, _ := strconv.Atoi(num)
+		if dir == "R" {
+			cv = cv + val
+		} else {
+			cv = cv - val
+		}
+
+		if cv == 0 {
+			counter++
+			fmt.Println(counter)
+		}
+
+	}
+
+	err = os.WriteFile("output.txt", items, 0777)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
